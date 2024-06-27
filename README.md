7c9c0d4f46090b3a3051fc6760dfaaf5addefa6f
MINABE Oki <minabe.oki@gmail.com>
2024-06-27 18:25:33 +0900

    evil-ime first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# evil-ime.el
+
+## What?
+
+Emacsで[Evil](https://github.com/emacs-evil/evil)を使いながら日本語を
+書く場合、Evilのモード切り替え(normal, insertなど)と、IMEのON/OFFの
+両方を意識して、入力操作をする必要があります。
+
+Evilのnormalモードから日本語テキストを入力して、その後normalモードに
+戻るには、
+
+1. `i`でEvilのinsert modeにする
+2. IMEをONにする
+3. 日本語を入力する
+4. IMEをOFFにする
+5. `ESC`でEvilのnormal modeに戻る
+
+という手順が必要です。例えば上記の4.をせずにnormal modeに戻ると、
+`j``k`とかでカーソル移動しようとして、IMEに入力が取られて残念な
+ことになります。
+
+このevil-imeは、Evilのnormal modeでは自動でIMEをOFFにして、
+insert modeになった場合は以前のinsert mode時のIME状態に戻します。
+
+1. `i`でEvilのinsert modeにする
+   (以前のinsert modeでIME ONだったら、自動でIME ONになる)
+2. 日本語を入力する
+3. `ESC`でEvilのnormal modeに戻る
+   (IMEは自動でOFFになる)
+
+## Install
+
+.elファイルをload-pathの通ったディレクトリに置いて、
+~/.emacs.d/init.elに以下を書いてください。
+
+```
+(require 'evil-ime)
+```
---
+++ evil-ime.el
@@ -0,0 +1,93 @@
+;;; evil-ime.el --- Change the IME state depends on evil mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Oki Minabe
+
+;; Author: Oki Minabe <minabe.oki@gmail.com>
+;; Keywords: i18n, input method
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(defgroup evil-ime nil
+  "Change the IME state depends on evil mode."
+  :group 'evil
+  :prefix "evil-ime-")
+
+(defcustom evil-ime-ascii-mode "com.apple.keylayout.ABC"
+  "The IME mode name when IME is disabled."
+  :group 'evil-ime
+  :type 'string)
+
+(defvar evil-ime--last-buffer nil)
+
+(defvar-local evil-ime--saved-ime-state nil)
+
+(defun evil-ime--get-mode ()
+  "Get the current IME mode."
+  (when (fboundp 'mac-input-source)
+    (mac-input-source)))
+
+(defun evil-ime--set-mode (mode)
+  "Set the IME mode."
+  (when (fboundp 'mac-select-input-source)
+    (if mode
+        (mac-select-input-source mode))))
+
+(defun evil-ime--ascii-mode ()
+  "Select ASCII mode."
+  (when (fboundp 'mac-select-input-source)
+    (mac-select-input-source evil-ime-ascii-mode)))
+
+;;;###autoload
+(defun evil-ime-save-ime-state ()
+  "Save the current IME mode and select ASCII mode."
+  (setq evil-ime--saved-ime-state (evil-ime--get-mode))
+  (evil-ime--ascii-mode))
+
+;;;###autoload
+(defun evil-ime-restore-ime-state ()
+  "Restore the IME mode."
+  (evil-ime--set-mode evil-ime--saved-ime-state))
+
+;;;###autoload
+(defun evil-ime-adjust-ime-state ()
+  "Select the IME mode depends on current evil-mode."
+  (unless (eq evil-ime--last-buffer (current-buffer))
+    (setq evil-ime--last-buffer (current-buffer))
+    (if (memq evil-state '(insert emacs))
+        (message "restore ime")
+      (message "ascii mode"))
+    (if (memq evil-state '(insert emacs))
+        (evil-ime-restore-ime-state)
+      (evil-ime--ascii-mode))))
+
+;;;###autoload
+(defun evil-ime-change-ime-state ()
+  "Save the IME mode when the IME mode is changed."
+  (if (memq evil-state '(insert emacs))
+      (setq evil-ime--saved-ime-state (evil-ime--get-mode))))
+
+(with-eval-after-load 'evil
+  (add-hook 'evil-insert-state-exit-hook #'evil-ime-save-ime-state)
+  (add-hook 'evil-insert-state-entry-hook #'evil-ime-restore-ime-state)
+  ;(add-hook 'buffer-list-update-hook #'evil-ime-adjust-ime-state)
+  (add-hook 'window-configuration-change-hook #'evil-ime-adjust-ime-state)
+  (add-hook 'mac-selected-keyboard-input-source-change-hook
+            #'evil-ime-change-ime-state))
+
+(provide 'evil-ime)
+;;; evil-ime.el ends here
